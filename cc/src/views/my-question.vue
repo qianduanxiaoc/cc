7434484674822cbe1b7407e6d206ff4d0e18d4c1
chencui <sz.chencui@dianmi365.com>
2018-11-16 18:23:29 +0800

    添加问题

--- cc/src/views/my-question.vue
+++ cc/src/views/my-question.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        
+        <p>window.moveTo\window.moveBy--8.1.4</p>
+        <p>window.screenTop\window.screenX\window.screenLeft\window.screenY</p>
+        <p>概念：视口（viewport），窗口</p>
+        <p>histroy.length --- 是否是当前窗口的第一个页面</p>
     </div>
 </template>
 <script>
