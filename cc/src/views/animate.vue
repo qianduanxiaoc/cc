ab1997a05582839bef063c3dae9091e367938a18
chencui <sz.chencui@dianmi365.com>
2018-11-23 16:44:00 +0800

    动画

--- cc/src/views/animate.vue
+++ cc/src/views/animate.vue
@@ -1,10 +1,20 @@
 <template>
     <div class="animate">
-        <span @click="search" >
-            se<span :class="{circle: true, active: isSearch}">
-                <input type="text" v-if="isSearch" v-focus>
-            </span>rch
-        </span>
+        <div class="search">
+            <span @click="search" >
+                se<span :class="{circle: true, active: isSearch}">
+                    <input type="text" v-if="isSearch"v-focus @blur="blur" @focus="focus">
+                </span>rch
+            </span>
+        </div>
+        
+        <!-- 参考 -->
+        <!-- https://developer.mozilla.org/zh-CN/docs/Web/CSS/clip-path -->
+        <div class="clip-path-wave">
+            <div class="wave wave1"></div>
+            <div class="wave wave2"></div>
+             <div class="wave wave3"></div>
+        </div>
     </div>
 </template>
 <script>
@@ -13,7 +23,8 @@
         components: {},
         data() {
             return {
-                isSearch: false
+                isSearch: false,
+                searchBlur: false
             }
         },
         computed: {
@@ -27,16 +38,27 @@
             }
         },
         methods: {
-            search(e) {
-                if(!this.isSearch) {
-                    this.isSearch = !this.isSearch
-                }
+            search() {
+                this.isSearch = true
+            },
+            focus() {
+                this.searchBlur = false
+                this.isSearch = true
+            },
+            blur() {
+                this.searchBlur = true
+                this.isSearch = false
             }
         }
     }
 </script>
 <style lang="scss" scope>
     .animate {
+        >div {
+            margin-bottom: 32px;
+        }
+    }
+    .search {
         >span {
             display: inline-block;
             font-size: 40px;
@@ -73,6 +95,85 @@
                 width: 122px;
                 padding: 0 11px;
             }
+        }
+    }
+
+    .clip-path-wave {
+        position: relative;
+        width: 300px;
+        height: 300px;
+        border-radius: 50%;
+        overflow: hidden;
+        border: 1px solid #efefef;
+
+        .wave {
+            position: absolute;
+            height: 500px;
+            width: 400px;
+            background: rgba(213,163,187,0.3);
+        }
+        .wave1 {
+            top: 60px;
+            left: -10px;
+            clip-path: ellipse(60% 50%);
+            animation: wave1 7s infinite;
+        }
+        .wave2 {
+            top: 30px;
+            left: -20%;
+            clip-path: ellipse(55% 50%);
+            animation: wave2 7s infinite;
+        }
+        .wave3 {
+            top: 80px;
+            left: -30%;
+            clip-path: ellipse(55% 50%);
+            animation: wave3 7s infinite;
+        }
+    }
+
+    @keyframes wave1 {
+        0% {
+            top: 60px;
+            left: -10px;
+        }
+        50% {
+            top: 50px;
+            left: -70px;
+        }
+        100% {
+            top: 60px;
+            left: -10px;
+        }
+    }
+
+    @keyframes wave2 {
+        0% {
+            top: 30px;
+            left: -80px;
+        }
+        50% {
+            top: 90px;
+            left: 20px;
+        }
+        100% {
+            top: 30px;
+            left: -80px;
+        }
+    }
+
+    @keyframes wave3 {
+        0% {
+            top: 80px;
+            left: -30px;
+        }
+        50% {
+            top: 40px;
+            left: -20px;
+        }
+        100% {
+            top: 80px;
+            left: -30px;
         }
     }
 </style>
